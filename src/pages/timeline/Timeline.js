a74e9cfb7b2c9413109424910b43348f98f7fdb2
devkgn88 <devkgn88@gmail.com>
2025-04-30 16:39:17 +0900

    [김가남] 타임라인 상단부 버튼 구성

--- src/pages/timeline/Timeline.js
+++ src/pages/timeline/Timeline.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 
 // components
 import PageTitle from "../../components/PageTitle";
+import TimelineController from "./TimelineController";
 
-export default function Tables() {
+export default function Timeline() {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateChange = (offset) => {
+        const newDate = new Date(selectedDate);
+        newDate.setDate(newDate.getDate() + offset);
+        setSelectedDate(newDate);
+    };
+
+    const formatTitle = (date) => {
+        const days = ['일', '월', '화', '수', '목', '금', '토'];
+        return `${date.getMonth() + 1}월 ${date.getDate()}일 (${days[date.getDay()]})`;
+    };
     return (
         <>
-        <PageTitle title="타임라인" />
+        <PageTitle title="예약현황" />
         <Grid container spacing={4}>
             <Grid item xs={12}>
-
+            <TimelineController
+                onDateChange={handleDateChange}
+                currentDate={formatTitle(selectedDate)}
+            />
             </Grid>
-
         </Grid>
         </>
     );
---
+++ src/pages/timeline/style.js
@@ -0,0 +1,31 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles(theme => ({
+    timelineController: {
+        display: "flex",
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+        padding: 20,
+    },
+    dashedBorder: {
+        border: "1px dashed",
+        borderColor: theme.palette.primary.main,
+        padding: theme.spacing(2),
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+        marginTop: theme.spacing(1),
+    },
+    dateChangeButton: {
+        color: "white",
+        textTransform: "none",
+        minWidth: 40,
+        lineHeight: '1.48 !important',
+    },
+    todayDateElement: {
+        color: '#6E6E6E',
+        fontSize: "1.285rem !important",
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+    },
+}));
